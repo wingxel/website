77cdfd20011f00b860053a7e1aff00004ab4fffb
wingxel <wingxel@gmail.com>
2021-12-09 23:09:40 +0300

    Update Content

--- res4/js/app.js
+++ res4/js/app.js
@@ -4,6 +4,9 @@ $(() => {
     const form = document.getElementById("search_form");
     let not_found = document.getElementById("result");
 
+    let d = document.getElementById("dt_d");
+    d.textContent = new Date().getFullYear();
+
     form.addEventListener("submit", event => {
         let query = form.elements.search_fm.value.trim();
         if (query.length !== 0) {
