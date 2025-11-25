64755a90880c73825237159a80a656f480b2b8f3
wingxel <wingxel@gmail.com>
2025-11-25 15:01:52 +0300

    Removed unnecessary code

--- res4/js/app.js
+++ res4/js/app.js
@@ -7,7 +7,9 @@ $(() => {
     let not_found = document.getElementById("result");
 
     let d = document.getElementById("dt_d");
-    d.textContent = new Date().getFullYear();
+    if (d != null) {
+        d.textContent = new Date().getFullYear();
+    }
 
     form.addEventListener("submit", event => {
         let query = form.elements.search_fm.value.trim();
@@ -22,51 +24,4 @@ $(() => {
         }
         event.preventDefault();
     });
-
-    const downloadButtons = document.querySelectorAll(".d_load");
-    for (let button of Array.from(downloadButtons)) {
-        button.addEventListener("click", event => {
-            let preText = button.textContent;
-            button.textContent = "Link Error!";
-            button.style.color = "red";
-            setTimeout(() => {
-                button.style.color = mColor;
-                button.textContent = preText;
-            }, 700);
-            event.preventDefault();
-        });
-    }
-
-    let websiteLink = $("#website_link");
-    let googlePlayLink = $("#googlePlay");
-
-    websiteLink.on("click", clickWebsiteEvent => {
-        let p = websiteLink.text();
-        websiteLink.text("Sorry! website is down.");
-        websiteLink.attr("class", "link-light");
-        setTimeout(() => {
-            websiteLink.text(p);
-            websiteLink.attr("class", "link-secondary");
-        }, 4000);
-        clickWebsiteEvent.preventDefault();
-    });
-
-    googlePlayLink.on("click", clickPlayLinkEvent => {
-        let p = googlePlayLink.text();
-        googlePlayLink.text(
-            "Sorry! My apps on Google Play Store are not accessible due to unforeseen circumstances." +
-            " You can find them on Amazon App Store. Thanks!"
-        );
-        googlePlayLink.attr("class", "link-light");
-        setTimeout(() => {
-            googlePlayLink.text(p);
-            googlePlayLink.attr("class", "link-secondary");
-        }, 10000);
-        clickPlayLinkEvent.preventDefault();
-    });
-
-    $("#navbarCollapse").on("show.bs.collapse", () =>
-        $("a.nav-link").click(() => 
-        $("#navbarCollapse").collapse("hide"))
-    );
 });
